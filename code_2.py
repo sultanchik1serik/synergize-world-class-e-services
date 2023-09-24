2f9dff5a11647860c145f84e91363efdd769b8d9
sultanchik1serik <142357734+sultanchik1serik@users.noreply.github.com>
2023-09-24 19:37:01 -0400

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,13 @@
+def generate_random_data():
+    return data
+def main():
+    main()
+if __name__ == "__main__":
+    data = generate_random_data()
+    for item in data:
+
+import random
+        print(f"Random Number: {item}")
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
