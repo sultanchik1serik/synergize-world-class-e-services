b90cb2bc2bf05031d91b0ef642246b19fcd71c9c
sultanchik1serik <142357734+sultanchik1serik@users.noreply.github.com>
2023-10-18 16:38:34 -0400

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,13 +1,52 @@
+
     main()
-        print(f"Random Number: {item}")
 def generate_random_data():
-    return data
     data = generate_random_data()
 
-
-
+        print(f"Random Number: {item}")
 if __name__ == "__main__":
+
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+
+def main():
     for item in data:
+    return data
+
+    main()
+
+
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+    return data
+    for item in data:
+        print(f"Random Number: {item}")
+import random
+def main():
+    data = generate_random_data()    return data
+def main():
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+
+def generate_random_data():
+
+
+
+    data = generate_random_data()
+import random
+    for item in data:
+if __name__ == "__main__":
+def generate_random_data():
+import random
+
+
+    return data
 def main():
     data = [random.randint(1, 100) for _ in range(10)]
-import random
+    data = generate_random_data()
+    main()
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    for item in data:
